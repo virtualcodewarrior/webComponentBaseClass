57b639094cff2cb4033a274d742167c60b8d6974
Rodney Draaisma <rodney@mainframe.lan>
2018-06-27 18:06:25 -0400

    Update build to make proper source maps

--- build.js
+++ build.js
@@ -9,10 +9,16 @@ const outputPath = 'dist';
 
 fs.ensureDirSync(outputPath);
 const walker = walk.walk(inputPath, { followLinks: false });
+fs.emptyDirSync(outputPath);
 
 walker.on('file', (p_Root, p_Stat, p_Next) => {
-	const info = minify(fs.readFileSync(path.resolve(p_Root, p_Stat.name)), { mangle: { keepClassName: true } }, { sourceType: 'module', sourceMaps: true });
-	fs.writeFileSync(path.resolve(outputPath, p_Stat.name), info.code);
+	const fileContent = fs.readFileSync(path.resolve(p_Root, p_Stat.name));
+	const info = minify(fileContent, { mangle: { keepClassName: true } }, { sourceType: 'module', sourceMaps: true });
+	// for some reason babel doesn't add the source map line, so do it here manually
+	fs.writeFileSync(path.resolve(outputPath, p_Stat.name), `${info.code}\n//@ sourceMappingURL=${p_Stat.name}.map`);
+	// for some reason babel doesn't do this, even if I set the appropriate options
+	// so set the source here manually
+	info.map.sources[0] = p_Stat.name;
 	fs.writeFileSync(path.resolve(outputPath, `${p_Stat.name}.map`), JSON.stringify(info.map));
 	p_Next();
 });
