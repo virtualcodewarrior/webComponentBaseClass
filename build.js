d74dde03618a4aef5b7868751ae5ee3134d64cb4
Rodney Draaisma <rodney@mainframe.lan>
2018-06-26 22:58:13 -0400

    try to add build minification step

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 /* eslint-env node */
-const babel = require('babel-core');
+const minify = require('babel-minify');
 const fs = require('fs-extra');
 const walk = require('walk');
 const path = require('path');
@@ -11,6 +11,8 @@ fs.ensureDirSync(outputPath);
 const walker = walk.walk(inputPath, { followLinks: false });
 
 walker.on('file', (p_Root, p_Stat, p_Next) => {
-	fs.writeFileSync(path.resolve(outputPath, p_Stat.name), babel.transform(fs.readFileSync(path.resolve(p_Root, p_Stat.name))));
+	const info = minify(fs.readFileSync(path.resolve(p_Root, p_Stat.name)), { mangle: { keepClassName: true } }, { sourceType: 'module', sourceMaps: true });
+	fs.writeFileSync(path.resolve(outputPath, p_Stat.name), info.code);
+	fs.writeFileSync(path.resolve(outputPath, `${p_Stat.name}.map`), JSON.stringify(info.map));
 	p_Next();
 });
