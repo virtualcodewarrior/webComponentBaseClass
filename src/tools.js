7ac3c723bdbcd46cd1b878064668b91e3b425b65
Rodney Draaisma <virtualcodewarrior@hotmail.com>
2019-02-10 10:56:37 -0500

    updated docs

--- src/tools.js
+++ src/tools.js
@@ -15,7 +15,7 @@ export function camelCaseToDashes(p_String) { return p_String.replace(/[a-z]?[A-
 /**
  * Get the child elements of the given HTMLElement and finds the attribute using the given key. It then puts all those objects in an object for quick access
  * This only will give the expected results if all values for the keys are unique
- * @param {HTMLElement|DocumentFragment|ShadowRoot} p_HTMLElement The HTMElement that contains the children for which we want to retrieve the keys
+ * @param {HTMLElement|DocumentFragment|Document|ShadowRoot} p_HTMLElement The HTMElement that contains the children for which we want to retrieve the keys
  * @param {string} p_Tag The attribute tag for which we are building this object (e.g. 'id')
  * @returns {object} An object where each member references the HTMLElement that had the attribute with the given tag, attributes with dashes are converted to camel case
  */
